6c8eb9ea1f11956f7393dfa695799443ea40421e
NattanGama <91083215+NattanGama@users.noreply.github.com>
2021-12-04 21:03:27 -0300

    Create Classe.cs

---
+++ Classe.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp {
+
+   class Pessoa
+{
+    public string Nome {get; set;}     
+    public string Sobrenome {get; set;}                                                                                        
+
+    public int Idade {get; set;} 
+}
+}
